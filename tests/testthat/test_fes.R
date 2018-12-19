2a2b4673bd235812d9b9d73aafcb5bd7b912d86b
Vojtech Spiwok <spiwokv@vscht.cz>
2018-12-19 18:26:44 +0100

    Update test_fes.R

--- tests/testthat/test_fes.R
+++ tests/testthat/test_fes.R
@@ -1,3 +1,6 @@
-myfes<-fes(acealanme, imax=2000)
-expect_identical(sum(myfes$fes), -1127743)
+context("Testing fes function")
+test_that("Testing that fes calculates correctly sum of points", {
+  myfes<-fes(acealanme, imax=2000)
+  expect_identical(sum(myfes$fes), -1127743)
+})
 
