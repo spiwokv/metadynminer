0fe533d8f0a39b340705763621233969d7b9e82d
Vojtech Spiwok <spiwokv@vscht.cz>
2018-12-19 22:16:10 +0100

    Update test_fes.R

--- tests/testthat/test_fes.R
+++ tests/testthat/test_fes.R
@@ -1,31 +1,36 @@
 context("Testing fes function")
 test_that("Testing that fes calculates correctly sum of points", {
-  
   # fes function 2D
   myfes<-fes(acealanme, imax=2000)
   summyfes<-sum(myfes$fes)
-  expect_equal(object=summyfes, expected=-1127743, tolerance=1, scale = 1)
+  expect_equal(object=summyfes, expected=-1127743, tolerance=1, scale=1)
   
   # fes function 2D
   myfes<-fes2(acealanme, imax=2000)
   summyfes<-sum(myfes$fes)
-  expect_equal(object=summyfes, expected=-1127743, tolerance=1, scale = 1)
+  expect_equal(object=summyfes, expected=-1127743, tolerance=1, scale=1)
   
   # fes function 1D
   myfes<-fes(acealanme1d, imax=2000)
   summyfes<-sum(myfes$fes)
-  expect_equal(object=summyfes, expected=-14790, tolerance=1, scale = 1)
+  expect_equal(object=summyfes, expected=-14790, tolerance=1, scale=1)
   
   # fes function 1D
   myfes<-fes2(acealanme1d, imax=2000)
   summyfes<-sum(myfes$fes)
-  expect_equal(object=summyfes, expected=-14790, tolerance=1, scale = 1)  
+  expect_equal(object=summyfes, expected=-14790, tolerance=1, scale=1)
 
   #+-*/
-  expect_equal(object=summyfes+summyfes, expected=-29581, tolerance=1, scale = 1)  
-  expect_equal(object=summyfes-summyfes, expected=0, tolerance=1, scale = 1)  
-  expect_equal(object=2*summyfes, expected=-29581, tolerance=1, scale = 1)  
-  expect_equal(object=summyfes/0.5, expected=-29581, tolerance=1, scale = 1)  
+  expect_equal(object=summyfes+summyfes, expected=-29581, tolerance=1, scale=1)
+  expect_equal(object=summyfes-summyfes, expected=0, tolerance=1, scale=1)
+  expect_equal(object=2*summyfes, expected=-29581, tolerance=1, scale=1)
+  expect_equal(object=summyfes/0.5, expected=-29581, tolerance=1, scale=1)
 
+  # fes2d21d
+  myfes<-fes(acealanme, imax=2000)
+  myfes2<-fes2d21d(myfes)
+  summyfes<-sum(myfes2$fes)
+  expect_equal(object=summyfes, expected=-1127743, tolerance=1, scale=1)
+  
 })
 
