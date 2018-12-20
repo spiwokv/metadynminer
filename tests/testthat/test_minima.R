4782f196a16ca6a537c2f616fd83e9adaeb39017
Vojtech Spiwok <spiwokv@vscht.cz>
2018-12-20 10:37:28 +0100

    Update test_minima.R

--- tests/testthat/test_minima.R
+++ tests/testthat/test_minima.R
@@ -1,10 +1,24 @@
 context("Testing fesminima and related functions")
 test_that("Testing that fesminima correctly identifies energy minima", {
-  # fesminima
+  # fesminima 2D
   myfes<-fes(acealanme, imax=2000)
   mins<-fesminima(myfes)
   minA <- mins$minima[1,6]
   expect_equal(object=minA, expected=-41.50, tolerance=0.02, scale=1)
+
+  # summary
+  minA <- summary(mins)[1,8]
+  expect_equal(object=minA, expected=38.3, tolerance=0.1, scale=1)
   
+  # fesminima 1D
+  myfes<-fes(acealanme1d, imax=2000)
+  mins<-fesminima(myfes)
+  minA <- mins$minima[1,4]
+  expect_equal(object=minA, expected=-72.12, tolerance=0.02, scale=1)
+
+  # summary
+  minA <- summary(mins)[1,6]
+  expect_equal(object=minA, expected=40.8, tolerance=0.1, scale=1)
+
 })
 
