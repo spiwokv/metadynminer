c01058d7b841e4735167914c2a09ed5bc7d213aa
Vojtech Spiwok <spiwokv@vscht.cz>
2022-04-12 14:13:48 +0200

    Added tests for fespoint

--- tests/testthat/test_minima.R
+++ tests/testthat/test_minima.R
@@ -94,5 +94,39 @@ test_that("Testing that fesminima correctly identifies energy minima", {
   minA<-summary(profs)[2,6]
   expect_equal(object=minA, expected=6.01, tolerance=0.01, scale=1)
 
+  # fespoint 2D
+  point<-fespoint(acealanme, c(3,3), imax=2000)
+  expect_equal(object=minA, expected=-17.41, tolerance=0.01, scale=1)
+
+  # fespoint 1D
+  point<-fespoint(acealanme1d, c(3), imax=2000)
+  expect_equal(object=minA, expected=-51.43, tolerance=0.01, scale=1)
+
+  # fespoint 2D with pertc(T,F)
+  acealanme2<-acealanme
+  acealanme2$per<-c(T,F)
+  point<-fespoint(acealanme2, c(3,3), imax=2000)
+  expect_equal(object=minA, expected=-13.26, tolerance=0.01, scale=1)
+
+  # fespoint 2D with pertc(F,T)
+  acealanme2<-acealanme
+  acealanme2$per<-c(F,T)
+  point<-fespoint(acealanme2, c(3,3), imax=2000)
+  expect_equal(object=minA, expected=-7.35, tolerance=0.01, scale=1)
+
+  # fespoint 2D with pertc(F,F)
+  acealanme2<-acealanme
+  acealanme2$per<-c(F,F)
+  point<-fespoint(acealanme2, c(3,3), imax=2000)
+  expect_equal(object=minA, expected=-6.61, tolerance=0.01, scale=1)
+
+  # fespoint 1D with pertc(F)
+  acealanme1d2<-acealanme1d
+  acealanme1d2$per<-c(F)
+  point<-fespoint(acealanme1d2, c(3), imax=2000)
+  expect_equal(object=minA, expected=-29.46, tolerance=0.01, scale=1)
+
+
+  
 })
 
